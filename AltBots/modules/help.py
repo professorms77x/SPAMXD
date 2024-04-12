25844cdaf0720572a05ea4099cecfa17710258fb
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2024-04-12 19:44:30 +0530

    Update help.py

--- AltBots/modules/help.py
+++ AltBots/modules/help.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
 
 
-HELP_STRING = f"★ 𝗦𝗣𝗔𝗠𝗫𝗗 𝗛𝗘𝗟𝗣 𝗠𝗘𝗡𝗨 ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @ALONE_WAS_BOT**"
+HELP_STRING = f"★ 𝗦𝗣𝗔𝗠𝗫𝗗 𝗛𝗘𝗟𝗣 𝗠𝗘𝗡𝗨 ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @sage_xd**"
 
 HELP_BUTTON = [
     [
@@ -14,8 +14,8 @@ HELP_BUTTON = [
       Button.inline("• ᴇxᴛʀᴀ •", data="extra")
     ],
     [
-      Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/AloneXBots"),
-      Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/AlonesHeaven")
+      Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/team_sage_xd"),
+      Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/sage_bot_support")
     ]
   ]
 
@@ -60,7 +60,7 @@ extra_msg = f"""
   2) {hl}leave : Type in the Group bot will auto leave that group
 
 
-**© @ALONE_WAS_BOT**
+**© @sage_xd**
 """
 
                  
@@ -92,7 +92,7 @@ raid_msg = f"""
   2) {hl}craid <count> <reply to user>
 
 
-**© @ALONE_WAS_BOT**
+**© @sage_xd**
 """
 
 spam_msg = f"""
@@ -109,7 +109,7 @@ spam_msg = f"""
   1) {hl}hang <counter>
 
 
-** © @ALONE_WAS_BOT**
+** © @sage_xd**
 """                     
            
            
@@ -136,13 +136,13 @@ async def helpback(event):
                 Button.inline("• ᴇxᴛʀᴀ •", data="extra")
               ],
               [
-                Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/AloneXBots"),
-                Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/AlonesHeaven")
+                Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/team_sage_xd"),
+                Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/sage_bot_support")
               ]
             ]
           )
     else:
-        await event.answer("Make Your Own Altron Bots !! @ALONE_WAS_BOT", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @sage_xd", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"spam"))
@@ -161,7 +161,7 @@ async def help_spam(event):
               buttons=[[Button.inline("< Back", data="help_back"),],],
               ) 
     else:
-        await event.answer("Make Your Own Altron Bots !! @ALONE_WAS_BOT", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @sage_xd", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"raid"))
@@ -180,7 +180,7 @@ async def help_raid(event):
             buttons=[[Button.inline("< Back", data="help_back"),],],
           )
     else:
-        await event.answer("Make Your Own Altron Bots !! @ALONE_WAS_BOT", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @sage_xd", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"extra"))
@@ -199,4 +199,4 @@ async def help_extra(event):
             buttons=[[Button.inline("< Back", data="help_back"),],],
             )
     else:
-        await event.answer("Make Your Own Altron Bots !! @ALONE_WAS_BOT", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @sage_xd", cache_time=0, alert=True)
