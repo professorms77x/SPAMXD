8c133ee7cf7ab9df6ae44e454337ab8d5f708d68
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2024-05-01 15:41:44 +0530

    Update bot.py

--- AltBots/modules/bot.py
+++ AltBots/modules/bot.py
@@ -1,10 +1,11 @@
-import os
 import sys
 import heroku3
-from datetime import datetime
-from config import X1, X2, X3, X4, X5 , X6, X7, X8, X9, X10, OWNER_ID, SUDO_USERS, HEROKU_APP_NAME, HEROKU_API_KEY, CMD_HNDLR as hl
-from telethon.tl.functions.users import GetFullUserRequest
+
+from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, OWNER_ID, SUDO_USERS, HEROKU_APP_NAME, HEROKU_API_KEY, CMD_HNDLR as hl
+
+from os import execl, getenv
 from telethon import events
+from datetime import datetime
 
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
@@ -20,10 +21,10 @@ from telethon import events
 async def ping(e):
     if e.sender_id in SUDO_USERS:
         start = datetime.now()
-        altron = await e.reply(f"•[ 🍹 ηєχz тум 🍹 ]•", parse_mode=None, link_preview=None)
+        altron = await e.reply(f"» __ᴀʟᴏɴᴇ__")
         end = datetime.now()
         mp = (end - start).microseconds / 1000
-        await altron.edit(f"[🍹] ηєχz ιѕ нєʀє\n[🏓] αвє αв тєʀα куα нσgα\n[⚡] кιѕкι ᴄнυ∂αι кαʀиι нαι\n\n➜ `{mp} ms`")
+        await altron.edit(f"__🤖 ᴘɪɴɢ__\n» `{mp} ᴍꜱ`")
 
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
@@ -38,7 +39,7 @@ async def ping(e):
 @X10.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
 async def restart(e):
     if e.sender_id in SUDO_USERS:
-        await e.reply(f"[🛸] ʀєвσσт ∂σиє\n[🍷] 2 мιит ωαιт ρℓєαѕє\n[🍁] fιʀ ααυиgα тєʀι мα ᴄнσ∂иє")
+        await e.reply(f"`ʀᴇsᴛᴀʀᴛɪɴɢ ʙᴏᴛ...`")
         try:
             await X1.disconnect()
         except Exception:
@@ -80,12 +81,8 @@ async def restart(e):
         except Exception:
             pass
 
-        os.execl(sys.executable, sys.executable, *sys.argv)
-        quit()
-        
+        execl(sys.executable, sys.executable, *sys.argv)
 
-Heroku = heroku3.from_key(HEROKU_API_KEY)
-sudousers = os.environ.get("SUDO_USER", None)
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
 @X2.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
@@ -99,8 +96,10 @@ sudousers = os.environ.get("SUDO_USER", None)
 @X10.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
 async def addsudo(event):
     if event.sender_id == OWNER_ID:
-        ok = await event.reply(f"🍃 мєʀα єк σʀ иєω вєтα α∂∂ нσ gαуα 🍃")
-        mks = "SUDO_USER"
+        Heroku = heroku3.from_key(HEROKU_API_KEY)
+        sudousers = getenv("SUDO_USERS", default=None)
+
+        ok = await event.reply(f"» __ᴀᴅᴅɪɴɢ ᴜꜱᴇʀ ᴀꜱ ꜱᴜᴅᴏ...__")
         target = ""
         if HEROKU_APP_NAME is not None:
             app = Heroku.app(HEROKU_APP_NAME)
@@ -113,21 +112,19 @@ async def addsudo(event):
         try:
             reply_msg = await event.get_reply_message()
             target = reply_msg.sender_id
-        except Exception:
-            await ok.edit("αвє נнαт кє вααℓ υραʀ ѕє ʀєρℓу ∂є ʀαнα нαι вααρ кσ")
-        if len(sudousers) > 0:
-            newsudo = f"{sudousers} {target}"
+        except:
+            await ok.edit("» ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ !!")
+            return
+
+        if str(target) in sudousers:
+            await ok.edit(f"ᴛʜɪꜱ ᴜꜱᴇʀ ɪꜱ ᴀʟʀᴇᴀᴅʏ ᴀ ꜱᴜᴅᴏ ᴜꜱᴇʀ !!")
         else:
-            newsudo = f"{target}"
-        await ok.edit(f":⧽ σує нσує мєʀα ᴄυтє вαᴄннα\n:⧽ `{target}`\n:⧽ `ωєℓᴄσмє тσ ηєχz ѕραм`")
-        heroku_var[mks] = newsudo   
-   
-     
-async def get_user(event):
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        if previous_message.forward:
-            replied_user = await event.client(GetFullUserRequest(previous_message.forward.sender_id))
-        else:
-            replied_user = await event.client(GetFullUserRequest(previous_message.sender_id))
-    return replied_user.user.id
+            if len(sudousers) > 0:
+                newsudo = f"{sudousers} {target}"
+            else:
+                newsudo = f"{target}"
+            await ok.edit(f"» **ɴᴇᴡ ꜱᴜᴅᴏ ᴜꜱᴇʀ**: `{target}`\n» `ʀᴇsᴛᴀʀᴛɪɴɢ ʙᴏᴛ...`")
+            heroku_var["SUDO_USERS"] = newsudo    
+    
+    elif event.sender_id in SUDO_USERS:
+        await event.reply("» ꜱᴏʀʀʏ, ᴏɴʟʏ ᴏᴡɴᴇʀ ᴄᴀɴ ᴀᴄᴄᴇꜱꜱ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ.")
