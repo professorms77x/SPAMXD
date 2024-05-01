fe42ed35ff9d03f133a4b2801ca00c205a3fe145
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2024-05-01 16:03:44 +0530

    Update help.py

--- AltBots/modules/help.py
+++ AltBots/modules/help.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
 
 
-HELP_STRING = f"★ 𝗦𝗣𝗔𝗠𝗫𝗗 𝗛𝗘𝗟𝗣 𝗠𝗘𝗡𝗨 ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @sage_xd**"
+HELP_STRING = f"★ 𝗦𝗣𝗔𝗠𝗫𝗗 𝗛𝗘𝗟𝗣 𝗠𝗘𝗡𝗨 ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @nexzz_op**"
 
 HELP_BUTTON = [
     [
@@ -14,8 +14,8 @@ HELP_BUTTON = [
       Button.inline("• ᴇxᴛʀᴀ •", data="extra")
     ],
     [
-      Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/team_sage_xd"),
-      Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/sage_bot_support")
+      Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/team_nexz"),
+      Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/nexz_support")
     ]
   ]
 
@@ -34,7 +34,7 @@ async def help(event):
     if event.sender_id in SUDO_USERS:
         try:
           await event.client.send_file(event.chat_id,
-              "https://telegra.ph//file/9e8ce3092848a1bc5d9d6.jpg",
+              "https://telegra.ph/file/816a63029c5ed26f3037a.jpg",
               caption=HELP_STRING,
               buttons=HELP_BUTTON
               )
@@ -60,7 +60,7 @@ extra_msg = f"""
   2) {hl}leave : Type in the Group bot will auto leave that group
 
 
-**© @sage_xd**
+**© @nexzz_op**
 """
 
                  
@@ -92,7 +92,7 @@ raid_msg = f"""
   2) {hl}craid <count> <reply to user>
 
 
-**© @sage_xd**
+**© @nexz_op**
 """
 
 spam_msg = f"""
@@ -109,7 +109,7 @@ spam_msg = f"""
   1) {hl}hang <counter>
 
 
-** © @sage_xd**
+** © @nexz_op**
 """                     
            
            
@@ -136,13 +136,13 @@ async def helpback(event):
                 Button.inline("• ᴇxᴛʀᴀ •", data="extra")
               ],
               [
-                Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/team_sage_xd"),
-                Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/sage_bot_support")
+                Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/team_nexz"),
+                Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/nexz_support")
               ]
             ]
           )
     else:
-        await event.answer("Make Your Own Altron Bots !! @sage_xd", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @nexzz_op", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"spam"))
@@ -161,7 +161,7 @@ async def help_spam(event):
               buttons=[[Button.inline("< Back", data="help_back"),],],
               ) 
     else:
-        await event.answer("Make Your Own Altron Bots !! @sage_xd", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @nexzz_op", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"raid"))
@@ -180,7 +180,7 @@ async def help_raid(event):
             buttons=[[Button.inline("< Back", data="help_back"),],],
           )
     else:
-        await event.answer("Make Your Own Altron Bots !! @sage_xd", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @nexzz_op", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"extra"))
@@ -199,4 +199,4 @@ async def help_extra(event):
             buttons=[[Button.inline("< Back", data="help_back"),],],
             )
     else:
-        await event.answer("Make Your Own Altron Bots !! @sage_xd", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @nexzz_op", cache_time=0, alert=True)
